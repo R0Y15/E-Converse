4ecfe1489c6a162f3fa5f50d31bd431b951bd866
Bishal Roy <bishalroy15jan@gmail.com>
2024-05-07 02:41:22 +0530

    MetaData Update

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import Sidebar from '@/components/Sidebar'
 import Navbar from '@/components/Navbar'
 import React, { ReactNode } from 'react'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: "E-Converse",
+    description: "The All New Video Conferencing App",
+    icons: {
+        icon: "/icons/logo.svg",
+    }
+};
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
     return (
