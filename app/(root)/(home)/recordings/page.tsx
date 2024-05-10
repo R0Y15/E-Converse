af6f11895e296ba59f58f270c3a174e700fd7880
Bishal Roy <bishalroy15jan@gmail.com>
2024-05-11 01:18:24 +0530

    Recordings page completed

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,12 +1,15 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Recordings = () => {
   return (
     <section className='felx size-full flex-col gap-10 text-white'>
-    <h1 className="text-3xl font-bold">
-      Recordings
-    </h1>
-  </section>
+      <h1 className="text-3xl font-bold">
+        Recordings
+      </h1>
+
+      <CallList type='recordings' />
+    </section>
   )
 }
 
