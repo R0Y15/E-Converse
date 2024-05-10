49c86e5f7a0adbaaa5c45fbc51b69e37649dea12
Bishal Roy <bishalroy15jan@gmail.com>
2024-05-11 01:22:23 +0530

    meetingprps and avatarimages exports

--- constants/index.ts
+++ constants/index.ts
@@ -52,4 +52,23 @@ export interface MeetingSetupProps {
     setupStatus: (value: boolean) => void;
 }
 
+export interface MeetingCardProps {
+    title: string,
+    date: string,
+    icon: string,
+    link: string,
+    isPreviousMeeting?: boolean,
+    buttonIcon1?: string,
+    buttonText?: string,
+    handleClick?: () => void,
+}
+
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+]
+
 export type CallLayoutTypes = 'speaker-left' | 'speaker-right' | 'grid' 
