ac894ba6f9d5fcc025800f06d825a3862bb32097
Bishal Roy <bishalroy15jan@gmail.com>
2024-05-11 01:17:49 +0530

    completed previous page

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,12 +1,15 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Previous = () => {
   return (
     <section className='felx size-full flex-col gap-10 text-white'>
-    <h1 className="text-3xl font-bold">
-      Previous
-    </h1>
-  </section>
+      <h1 className="text-3xl font-bold">
+        Previous
+      </h1>
+
+      <CallList type='ended' />
+    </section>
   )
 }
 
